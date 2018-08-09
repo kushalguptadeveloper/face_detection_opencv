7a9756145965b9f95ce14a70c1c82fa45c424c0f
Kushal Gupta <kushalguptadeveloper@gmail.com>
2018-08-09 22:24:22 +0530

    LBPHFaceRecognizer

--- trainer.py
+++ trainer.py
@@ -3,8 +3,34 @@ import cv2
 import numpy as np
 from PIL import Image
 
-recognizer = cv2.createLBPHFaceRecognizer()
+recognizer1 = cv2.face.FaceRecognizer()
+detector= cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
 path = '/Users/kushalgupta/face_detection_opencv/dataSet'
 
 def getImageWithId(path):
-    
+    imagePaths=[os.path.join(path,f) for f in os.listdir(path)]
+    faceSamples=[]
+    Ids=[]
+    for imagePath in imagePaths:
+        pilImage=Image.open(imagePath).convert('L')
+        imageNp=np.array(pilImage,'uint8')
+        Id=os.path.split(imagePath)[-1].split(".")[1]
+ #       faceSamples.append(imageNp)
+#        Ids.append(Id)
+        faces=detector.detectMultiScale(imageNp)
+        for (x,y,w,h) in faces:
+            faceSamples.append(imageNp[y:y+h,x:x+w])
+            Ids.append(Id)
+  
+ #       cv2.waitKey(10)
+    return Ids,faceSamples
+
+ids,faces = getImageWithId(path)
+detector.train(faces,np.array(ids)
+#recognizer1.save('recognizer/trainner.yml')
+recognizer1.write('/Users/kushalgupta/face_detection_opencv/recognizer/trainingData.yml')
+cv2.destroyAllWindows()
+               
+
+ 
+
