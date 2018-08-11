6865a65922a73f433889e204ffe9f0ef5d9db7b3
Kushal Gupta <kushalguptadeveloper@gmail.com>
2018-08-11 11:14:37 +0530

    detector bug correction

--- detector.py
+++ detector.py
@@ -5,7 +5,8 @@ cam = cv2.VideoCapture(0)
 rec = cv2.face.LBPHFaceRecognizer_create()
 rec.read('/Users/kushalgupta/face_detection_opencv/recognizer/trainingData.yml')
 id = 0
-font = cv2.cv.InitFont(cv2.cv.CV_FONT_HERSHEY_SIMPLEX,5,1,0,4)
+font = cv2.FONT_HERSHEY_SIMPLEX
+#font = cv2.cv.InitFont(cv2.cv.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 1, 1) 
 while(True):
     ret,img = cam.read()
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -13,7 +14,10 @@ while(True):
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
         id,conf = rec.predict(gray[y:y+h,x:x+w])
-        cv2.cv.PutText(cv2.cv.fromarray(img),str(id),(x,y+h),font,255)
+        flip_fr = cv2.flip(img,1)
+        cv2.putText(img,str(id),(x,y+h), font, 1, (200,255,155), 2, cv2.LINE_AA)
+#        cv2.putText(flip_fr, str(id), (x, y+h), font, 1,(255, 255, 0), 2)
+ #       cv2.putText(cv2.cv.fromarray(img),str(id),(x,y+h),font,255,2)
     cv2.imshow("Face",img)
     if cv2.waitKey(1) == ord('q'):
         break
